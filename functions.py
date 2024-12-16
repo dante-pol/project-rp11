71191cde1b623614508f027241e076f1744b252a
Dmitry-Karkarich-Rak <karkarich@seschool.ru>
2024-12-16 20:30:40 +0300

    fix(functions.calculate_area): add condition exist triangle

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 def calculate_area(a: float, b: float, c: float) -> float:
     import math
     
+    if a > b + c or b > c + a or c > a + b:
+        return None
+    
     p = (a + b + c) / 2
 
     area = math.sqrt(p * (p - a) * (p - b) * (p - c))
