b1b6258c753164c7f570e5a48905021c57ae8afe
Dmitry-Karkarich-Rak <karkarich@seschool.ru>
2024-12-16 21:19:20 +0300

    feat(functions): add validate text function

--- functions.py
+++ functions.py
@@ -1,3 +1,16 @@
+def validate_text(a:str, b:str, c:str):
+    if not a.replace("-", "").replace(".", "").isdigit():
+        return False
+    
+    if not b.replace("-", "").replace(".", "").isdigit():
+        return False
+    
+    if not c.replace("-", "").replace(".", "").isdigit():
+        return False
+    
+    return True
+
+
 def calculate_area(a: float, b: float, c: float) -> float:
     import math
     
