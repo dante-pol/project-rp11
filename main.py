12003ab21156c4cdd68244e438d69c4b7d755230
Dmitry-Karkarich-Rak <karkarich@seschool.ru>
2024-12-16 21:02:55 +0300

    feat(main): add main loop

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import math
 import functions
 
-a = int(input("input a >>"))
-b = int(input("input b >>"))
-c = int(input("input c >>"))
 
-area = functions.calculate_area(a, b, c)
+is_program_exit = False
 
-print(f"area= {area}")
+while not is_program_exit:
+
+    a = int(input("input a >>"))
+    b = int(input("input b >>"))
+    c = int(input("input c >>"))
+
+    area = functions.calculate_area(a, b, c)
+
+    print(f"area= {area}")
+
+    user_input = input("Y/N")
+
+    if user_input == "N":
+        is_program_exit = True
